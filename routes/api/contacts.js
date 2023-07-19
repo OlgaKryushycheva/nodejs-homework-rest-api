c75ddd25855a2712cf8e9a9082a5f1ce9f0c722a
OlgaKryushycheva <115803033+OlgaKryushycheva@users.noreply.github.com>
2023-07-19 19:30:42 +0300

    add isEmptyBody

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,7 +1,7 @@
 import express from "express";
 
 import ctrl from "../../controlers/contacts.js";
-import { validateBody } from "../../middlewares/index.js";
+import { validateBody, isEmptyBody } from "../../middlewares/index.js";
 import contactSchema from "../../schemas/contactSchema.js";
 
 const contactsRouter = express.Router();
@@ -10,10 +10,15 @@ contactsRouter.get("/", ctrl.getAll);
 
 contactsRouter.get("/:contactId", ctrl.getByID);
 
-contactsRouter.post("/", validateBody(contactSchema), ctrl.add);
+contactsRouter.post("/", isEmptyBody, validateBody(contactSchema), ctrl.add);
 
 contactsRouter.delete("/:contactId", ctrl.deleteByID);
 
-contactsRouter.put("/:contactId", validateBody(contactSchema), ctrl.updateByID);
+contactsRouter.put(
+  "/:contactId",
+  isEmptyBody,
+  validateBody(contactSchema),
+  ctrl.updateByID
+);
 
 export default contactsRouter;
